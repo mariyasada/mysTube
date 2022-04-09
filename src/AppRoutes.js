bef221a146a7d40741bc3a4421bd6ee0a5bc7dc5
mariyasada <mariyasada2406@gmail.com>
2022-04-09 15:13:33 +0530

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import {Route,Routes} from "react-router-dom";
-import { Home,  LogInPage, SignUpPage, VideoListPage } from "./Pages/index";
+import { Home,  LogInPage, SignUpPage, VideoListPage } from "./Pages";
 
 export const AppRoutes = () => {
   return (
-    <div>     
+       
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/videopage" element={<VideoListPage/>}/>
            <Route path="/loginpage" element={<LogInPage/>} />
            <Route path="/signup" element={<SignUpPage/>}/>
         </Routes>
-    </div>
+    
   )
 }
 
